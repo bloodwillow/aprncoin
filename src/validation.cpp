0372f23a9f7b2650ae7667f7540db96589d49761
bloodwillow <48611634+bloodwillow@users.noreply.github.com>
2023-10-10 03:54:57 -0700

    include boost/bind.hpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -48,6 +48,7 @@
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/algorithm/string/join.hpp>
 #include <boost/thread.hpp>
+#include <boost/bind.hpp>
 
 #if defined(NDEBUG)
 # error "aprncoin cannot be compiled without assertions."
